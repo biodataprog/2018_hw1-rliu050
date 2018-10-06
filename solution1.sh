de6c86d99c1a8142f89f621811e6373c9a4cfbd8
rui liu <ruiliu@ruis-MacBook-Pro.local>
2018-10-05 19:04:05 -0700

    homework1 done

--- solution1.sh
+++ solution1.sh
@@ -4,7 +4,7 @@
 
 # 1. Getting data
 
-curl -OL https://biodataprog.github.io/2018_programming-intro/data/Ecoli-vs-Yersinia.BLASTP.tab.gz
+curl -O https://biodataprog.github.io/2018_programming-intro/data/Ecoli-vs-Yersinia.BLASTP.tab.gz
 du -h Ecoli-vs-Yersinia.BLASTP.tab.gz
 
 # 2. Compressing and uncompressing
@@ -20,13 +20,18 @@ wc -l Ecoli-vs-Yersinia.BLASTP.tab
 
 # 4. Sorting
 
-curl -OL https://biodataprog.github.io/2018_programming-intro/data/Nc3H.expr.tab
+curl -O https://biodataprog.github.io/2018_programming-intro/data/Nc3H.expr.tab
 (head -n 1 Nc3H.expr.tab && tail -n +2 Nc3H.expr.tab | sort -nrk 6) > Nc20H.expr.sorted.tab
 head -n 10 Nc20H.expr.sorted.tab
 
 # 5. Finding and Counting
 
-grep -n "  CDS  " D_mel.63B12.gbk
-grep -c "  CDS  " D_mel.63B12.gbk
+grep -c "  CDS  " D_mel.63B12.gbk # number of CDS features
+awk '$3 > 99.99 {print $1,$3}' Ecoli-vs-Yersinia.BLASTP.tab | wc -l
+# or cut -f3 Ecoli-vs-Yersinia.BLASTP.tab | grep -c "100\.0"
+awk '$3 >= 90 {print $1,$3}' Ecoli-vs-Yersinia.BLASTP.tab | wc -l
 
-# to be continued
+# 6. Sort and Uniq
+
+curl -O https://biodataprog.github.io/2018_programming-intro/data/codon_table.txt
+cut -f3 codon_table.txt | uniq -c
